8e1e624d2a6e5d4c5d293f27e873752246d33ebe
Abel Mehedinteanu <me.abel52@gmail.com>
2023-10-20 18:12:18 +0300

    Add new functionality to gameboard

--- src/gameboard.js
+++ src/gameboard.js
@@ -21,6 +21,53 @@ function clear() {
     }
 }
 
+function getBoardState() {
+    // Check rows
+    for (let i = 0; i < 3; i++) {
+      if (board[i][0] === board[i][1] && board[i][1] === board[i][2] && board[i][0] !== '') {
+        return board[i][0];
+      }
+    }
+  
+    // Check columns
+    for (let i = 0; i < 3; i++) {
+      if (board[0][i] === board[1][i] && board[1][i] === board[2][i] && board[0][i] !== '') {
+        return board[0][i];
+      }
+    }
+  
+    // Check diagonals
+    if (board[0][0] === board[1][1] && board[1][1] === board[2][2] && board[0][0] !== '') {
+      return board[0][0];
+    }
+    if (board[0][2] === board[1][1] && board[1][1] === board[2][0] && board[0][2] !== '') {
+      return board[0][2];
+    }
+  
+    // Check for a tie
+    let isTie = true;
+    for (let i = 0; i < 3; i++) {
+      for (let j = 0; j < 3; j++) {
+        if (board[i][j] === '') {
+          isTie = false;
+          break;
+        }
+      }
+    }
+    if (isTie) {
+      return 'T';
+    }
+  
+    return null;
+}
+
+function getBoardCopy() {
+    return board.map(row => [...row]);
+}
+
+function logBoard() {
+    console.log(board);
+}
 
 
 function isCellOccupied(row, col) {
@@ -34,4 +81,8 @@ function isCellOccupied(row, col) {
 export {
     mark,
     clear,
+    isCellOccupied,
+    getBoardCopy,
+    logBoard,
+    getBoardState,
 }
