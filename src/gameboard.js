7d94a876a137174add51c015455c904c74eac18c
Abel Mehedinteanu <me.abel52@gmail.com>
2023-10-20 10:46:34 +0300

    Implement basic gameboard module

---
+++ src/gameboard.js
@@ -0,0 +1,37 @@
+let board = [
+    ['', '', ''],
+    ['', '', ''],
+    ['', '', '']
+];
+
+
+function mark(player, row, col) {
+    if (!isCellOccupied(row, col)) {
+        board[row - 1][col - 1] = player;
+    } else {
+        console.log(`Cannot mark: cell ${row}:${col} id occupied`);
+    }
+}
+
+function clear() {
+    for (let row = 0; row < 3; row++) {
+        for (let col = 0; col < 3; col++) {
+            board[row][col] = '';
+        }
+    }
+}
+
+
+
+function isCellOccupied(row, col) {
+    if (board[row - 1][col - 1] !== '') {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+export {
+    mark,
+    clear,
+}
